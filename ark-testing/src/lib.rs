ba36a788dbef40eb4d6336871aec1c027cf3918b
Erik De Smedt <edesmedt@blockstream.com>
2024-07-18 12:19:12 +0200

    Capture logs in test
    Captures all logs when running a test. By default, the logs will be
    displayed if the test failed.

--- ark-testing/src/lib.rs
+++ ark-testing/src/lib.rs
@@ -17,7 +17,17 @@ impl TestContext {
 
 	pub fn new(name: String, base_path: PathBuf) -> Self {
 		fs::create_dir_all(base_path.clone()).unwrap();
-		TestContext { name, datadir: base_path}
+		let context = TestContext { name, datadir: base_path};
+		context.init_logging().unwrap();
+		context
+	}
+
+	pub fn init_logging(&self) -> anyhow::Result<()> {
+		// We ignore the output
+		// An error is returned if the logger is initiated twice
+		// Note, that every test tries to initiate the logger
+		let _ = env_logger::builder().is_test(true).try_init();
+		Ok(())
 	}
 
 	pub fn generate() -> Self {
@@ -25,6 +35,7 @@ impl TestContext {
 		let datadir = ["/tmp/ark-testing/", &name].iter().collect();
 		Self::new(name, datadir)
 	}
+
 }
 
 impl Drop for TestContext {
@@ -32,12 +43,12 @@ impl Drop for TestContext {
 		// Remove the data-directory
 		// If the user has set `LEAVE_INTACT` we don't delete any 
 		// test-data.
-
 		if std::env::var(constants::env::TEST_LEAVE_INTACT).is_ok() {
+			log::info!("Leaving test-context intact at {:?}", self.datadir);
 			return
 		}
 		if self.datadir.exists() {
-			log::info!("Test clean-up");
+			log::trace!("Cleaning up test-context. Run again with `TEST_LEAVE_INTACT=1` to keep the test intact");
 			std::fs::remove_dir_all(self.datadir.clone()).unwrap();
 		}
 	}
@@ -56,7 +67,7 @@ mod test {
 				assert!(context.datadir.exists());
 				drop(context);
 
-				// The test cleans up after itself
+				// The test cleans up after itself if TEST_LEAVE_INTACT is not set
 				match std::env::var(constants::env::TEST_LEAVE_INTACT) {
 					Ok(_) => assert!(base_path.exists()),
 					Err(_) => assert!(!base_path.exists())
