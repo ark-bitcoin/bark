b66be3910c71036a898b1fbef911f6cefc285d49
Steven Roose <steven@stevenroose.org>
2024-07-26 09:24:47 +0100

    aspd: Move rpc struct conversion to dedicated module

--- aspd/src/rpcserver.rs
+++ aspd/src/rpcserver.rs
@@ -13,7 +13,7 @@ use ark::{musig, OffboardRequest, VtxoRequest, Vtxo, VtxoId};
 
 use crate::App;
 use crate::rpc;
-use crate::round::{RoundEvent, RoundInput};
+use crate::round::RoundInput;
 
 macro_rules! badarg {
 	($($arg:tt)*) => {{
@@ -175,53 +175,7 @@ impl rpc::ArkService for Arc<App> {
 
 		Ok(tonic::Response::new(Box::new(stream.map(|e| {
 			let e = e.map_err(|e| internal!("broken stream: {}", e))?;
-			Ok(rpc::RoundEvent {
-				event: Some(match e {
-					RoundEvent::Start { id, offboard_feerate } => {
-						rpc::round_event::Event::Start(rpc::RoundStart {
-							round_id: id,
-							offboard_feerate_sat_vkb: offboard_feerate.to_sat_per_kwu() * 4,
-						})
-					},
-					RoundEvent::VtxoProposal {
-						id, vtxos_spec, round_tx, vtxos_signers, vtxos_agg_nonces,
-					} => {
-						rpc::round_event::Event::VtxoProposal(rpc::VtxoProposal {
-							round_id: id,
-							vtxos_spec: vtxos_spec.encode(),
-							round_tx: bitcoin::consensus::serialize(&round_tx),
-							vtxos_signers: vtxos_signers.into_iter()
-								.map(|k| k.serialize().to_vec())
-								.collect(),
-							vtxos_agg_nonces: vtxos_agg_nonces.into_iter()
-								.map(|n| n.serialize().to_vec())
-								.collect(),
-						})
-					},
-					RoundEvent::RoundProposal { id, vtxos, round_tx, forfeit_nonces } => {
-						rpc::round_event::Event::RoundProposal(rpc::RoundProposal {
-							round_id: id,
-							signed_vtxos: vtxos.encode(),
-							round_tx: bitcoin::consensus::serialize(&round_tx),
-							forfeit_nonces: forfeit_nonces.into_iter().map(|(id, nonces)| {
-								rpc::ForfeitNonces {
-									input_vtxo_id: id.bytes().to_vec(),
-									pub_nonces: nonces.into_iter()
-										.map(|n| n.serialize().to_vec())
-										.collect(),
-								}
-							}).collect(),
-						})
-					},
-					RoundEvent::Finished { id, vtxos, round_tx } => {
-						rpc::round_event::Event::Finished(rpc::RoundFinished {
-							round_id: id,
-							signed_vtxos: vtxos.encode(),
-							round_tx: bitcoin::consensus::serialize(&round_tx),
-						})
-					},
-				})
-			})
+			Ok(e.into())
 		}))))
 	}
 
