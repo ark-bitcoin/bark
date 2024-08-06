9e57282d13d98aec5afad5b349cfbf2f2ee465fb
Steven Roose <steven@stevenroose.org>
2024-08-06 19:09:14 +0100

    testing: Custom log formatting, be a bit more concise

--- ark-testing/src/util.rs
+++ ark-testing/src/util.rs
@@ -12,10 +12,14 @@ pub fn init_logging() -> anyhow::Result<()> {
 	// We ignore the output
 	// An error is returned if the logger is initiated twice
 	// Note, that every test tries to initiate the logger
-	let _ =
-		env_logger::Builder::from_env(
-			env_logger::Env::default().default_filter_or("trace"))
-		.is_test(true)
-		.try_init();
+	let _ = fern::Dispatch::new()
+		.format(|out, msg, rec| {
+			let now = chrono::Local::now();
+			let stamp = now.format("%H:%M:%S.%3f");
+			out.finish(format_args!("[{} {: >5}] {}", stamp, rec.level(), msg))
+		})
+		.level(log::LevelFilter::Trace)
+		.chain(std::io::stdout())
+		.apply();
 	Ok(())
 }
