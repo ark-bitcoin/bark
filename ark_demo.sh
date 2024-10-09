e40698daf69834ee6acb8200c4d804b6bffd524d
adbd7e7 <adbd7e7@noreply.codeberg.org>
2024-10-09 10:22:24 +0000

    Added fallbackfee to the bd alias to match the documentation

--- ark_demo.sh
+++ ark_demo.sh
@@ -13,7 +13,7 @@ BITCOIND_COOKIE=$PWD/test/bitcoindatadir/regtest/.cookie
 mkdir -p ${BITCOIN_DATADIR}
 
 # Define useful aliases
-alias bcli="$BITCOIN_CLI -regtest --rpcport=$BITCOIND_RPC_PORT --rpccookiefile=$BITCOIND_COOKIE"
+alias bcli="$BITCOIN_CLI -regtest --rpcport=$BITCOIND_RPC_PORT --rpccookiefile=$BITCOIND_COOKIE -fallbackfee=0.0002"
 alias aspd="cargo run --bin aspd --"
 alias bark="cargo run --bin bark --"
 alias bd="$BITCOIND -regtest -datadir=${BITCOIN_DATADIR} -server -txindex"
