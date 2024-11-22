d59b6652c1a846a178f765ad67ec58777d1c5b8a
Steven Roose <steven@stevenroose.org>
2024-11-21 21:51:33 -0300

    nix: Clean up flake.nix
    - PROTOC env not needed
    - weird LLVM vars no longer needed now that we have dynamic ROCKSDB
      linking
    - add just

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 		nixpkgs.url = "nixpkgs/nixos-24.05";
 		flake-utils = {
 			url = "github:numtide/flake-utils";
-			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		rust-overlay = {
 			url = "github:oxalica/rust-overlay";
@@ -17,9 +16,9 @@
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				rustVersion = "1.77.1";
-				protobufVersion = "3.12.4";
 				bitcoinVersion = "28.0";
 				lightningVersion = "24.05";
+				protobufVersion = "3.12.4";
 
 				lib = nixpkgs.lib;
 				overlays = [ rust-overlay.overlays.default ];
@@ -53,54 +52,29 @@
 					};
 				});
 
-				target = pkgs.stdenv.buildPlatform.config;
-				target_underscores = lib.strings.replaceStrings [ "-" ] [ "_" ] target;
-				target_underscores_upper = lib.strings.toUpper target_underscores;
-
-				# This is a bunch of stuff that is somehow necessary to build rocksdb.
-				targetLlvmConfigWrapper = { binClangPkg, libClangPkg }: pkgs.writeShellScriptBin "llvm-config" ''
-					if [ "$1" == "--bindir" ]; then
-						echo "${binClangPkg}/bin"
-						exit 0
-					fi
-					if [ "$1" == "--prefix" ]; then
-						echo "${libClangPkg}"
-						exit 0
-					fi
-					exec llvm-config "$@"
-				'';
-				llvmConfigPkg = targetLlvmConfigWrapper {
-					binClangPkg = pkgs.llvmPackages.clang;
-					libClangPkg = pkgs.llvmPackages.clang-unwrapped.lib;
-				};
-				clang = pkgs.llvmPackages.clang;
+				target = lib.strings.replaceStrings [ "-" ] [ "_" ] pkgs.stdenv.buildPlatform.config;
 			in
 			{
 				devShells.default = pkgs.mkShell {
 					nativeBuildInput = [ ];
-					buildInputs = [ clang ] ++ [
+					buildInputs = [ pkgs.llvmPackages.clang ] ++ [
 						(pkgs.rust-bin.stable.${rustVersion}.default.override {
 							extensions = [ "rust-src" "rust-analyzer" ];
 						})
 						pkgs.pkg-config
-						protobuf
 						pkgs.sqlite
+						pkgs.just
+						protobuf
 						bitcoin
 						clightning
 					];
 
-					LLVM_CONFIG_PATH = "${llvmConfigPkg}/bin/llvm-config";
-					LLVM_CONFIG_PATH_native = "${llvmConfigPkg}/bin/llvm-config";
-					"LLVM_CONFIG_PATH_${target_underscores}" = "${llvmConfigPkg}/bin/llvm-config";
 					LIBCLANG_PATH = "${pkgs.llvmPackages.clang-unwrapped.lib}/lib/";
-					"CARGO_TARGET_${target_underscores_upper}_LINKER" = "${clang}/bin/clang";
-
-					PROTOC = "${protobuf}/bin/protoc";
 
 					ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib/";
-					"ROCKSDB_${target_underscores}_LIB_DIR" = "${pkgs.rocksdb}/lib/";
+					"ROCKSDB_${target}_LIB_DIR" = "${pkgs.rocksdb}/lib/";
 					#ROCKSDB_STATIC = "true"; # NB do this for prod
-					#"ROCKSDB_${target_underscores}_STATIC" = "true"; # NB do this for prod
+					#"ROCKSDB_${target}_STATIC" = "true"; # NB do this for prod
 
 					BITCOIND_EXEC = "${bitcoin}/bin/bitcoind";
 					LIGHTNINGD_EXEC = "${clightning}/bin/lightningd";
