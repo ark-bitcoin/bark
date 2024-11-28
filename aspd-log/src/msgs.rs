634daa242ab1cd2111a8f8ee886317d1a9ce79df
Steven Roose <steven@stevenroose.org>
2024-11-28 14:07:13 -0300

    aspd: Restart round if forfeit signatures are missing

--- aspd-log/src/msgs.rs
+++ aspd-log/src/msgs.rs
@@ -11,6 +11,7 @@
 use bitcoin::{Amount, OutPoint, Txid};
 use serde::{Deserialize, Serialize};
 
+use ark::VtxoId;
 
 
 // round flow
@@ -60,3 +61,15 @@ pub struct SweepComplete {
 }
 impl_slog!(SweepComplete, Info, "Completed a sweep tx");
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MissingForfeits {
+	pub round_id: u64,
+	pub input: VtxoId,
+}
+impl_slog!(MissingForfeits, Trace, "Missing forfeit sigs for input vtxo");
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RestartMissingForfeits {
+	pub round_id: u64,
+}
+impl_slog!(RestartMissingForfeits, Debug, "Restarting round because of missing forfeits");
