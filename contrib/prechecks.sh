fa5c71fe8c45c49af0027d619f9127847a177df1
Steven Roose <steven@stevenroose.org>
2025-01-07 10:39:50 +0000

    ci: Fix pre-checks script

--- contrib/prechecks.sh
+++ contrib/prechecks.sh
@@ -1,4 +1,5 @@
 
+
 # We don't want to run formatting on auto-generated files
 RUST_FILES="$(git ls-files *.rs \
   | git check-attr --stdin linguist-generated \
@@ -9,37 +10,41 @@ RUST_FILES="$(git ls-files *.rs \
 # We don't allow any line that starts with a whitespace
 function rust_no_spaces_for_indent() {
   for filename in $RUST_FILES; do
-    output=$(grep -n '^ ' $filename)
-    status_code=$?
+	output=$(grep -n '^ ' "$filename")
+	status_code=$?
 
-    if [ $status_code -eq 0 ]; then
-      echo "Format error in '$filename"
-      echo "$output"
-      exit 1
-    elif [ $status_code -eq 2 ]; then
-      echo "An error occurred"
-      echo $output
-      exit 2
-    fi
+	if [ $status_code -eq 0 ]; then
+	  echo "Format error in '$filename'"
+	  echo "$output"
+
+	  exit 1
+	elif [ $status_code -eq 2 ]; then
+	  echo "An error occurred"
+	  echo "$output"
+
+	  exit 2
+	fi
   done
 }
 
 # Check if there are structure log messages in aspd-logs that are not used.
 function unused_aspd_logs() {
 	ok=0
-	rg "pub struct " ./aspd-log/src/msgs | cut -d ' ' -f 3 | while read -r log; do
+
+	while read -r log; do
 		if ! rg -trust --quiet "slog.\($log" ./aspd/src/; then
-			echo "UNUSED: $log"
+			echo "UNUSED: '$log'"
 			ok=1
 		fi
-	done
-	exit "$ok"
+	done < <(rg "pub struct " ./aspd-log/src/msgs | cut -d ' ' -f 3)
+
+	exit $ok
 }
 
 # Check if the function exists and execute it
 if declare -f "$1" > /dev/null; then
-    "$@"
+	"$@"
 else
-    echo "Function '$1' not found!"
-    exit 1
+	echo "Function '$1' not found!"
+	exit 1
 fi
