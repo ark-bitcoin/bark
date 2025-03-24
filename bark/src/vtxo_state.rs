383b98cb2d61e8d0d5a3c0ea0c845a4a0280569b
Erik De Smedt <contact@erikdesmedt.be>
2025-03-24 13:35:11 +0100

    bark: Make VtxoState serializable

--- bark/src/vtxo_state.rs
+++ bark/src/vtxo_state.rs
@@ -5,7 +5,7 @@ const UNREGISTERED_BOARD : &'static str = "UnregisteredBoard";
 const READY: &'static str = "Ready";
 const SPENT: &'static str = "Spent";
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum VtxoState {
 	Ready,
 	Spent,
@@ -49,14 +49,32 @@ mod test {
 	#[test]
 	fn convert_vtxo_state_and_back() {
 		// From str to vtxostate
-		assert_eq!(VtxoState::from_str("Ready").unwrap(), VtxoState::Ready);
-		assert_eq!(VtxoState::from_str("Spent").unwrap(), VtxoState::Spent);
-		assert_eq!(VtxoState::from_str("UnregisteredBoard").unwrap(), VtxoState::UnregisteredBoard);
+		assert_eq!(VtxoState::from_str(READY).unwrap(), VtxoState::Ready);
+		assert_eq!(VtxoState::from_str(SPENT).unwrap(), VtxoState::Spent);
+		assert_eq!(VtxoState::from_str(UNREGISTERED_BOARD).unwrap(), VtxoState::UnregisteredBoard);
 
 		// From VtxoState to str
-		assert_eq!(VtxoState::Ready.as_str(), "Ready");
-		assert_eq!(VtxoState::Spent.as_str(), "Spent");
-		assert_eq!(VtxoState::UnregisteredBoard.as_str(), "UnregisteredBoard");
+		assert_eq!(VtxoState::Ready.as_str(), READY);
+		assert_eq!(VtxoState::Spent.as_str(), SPENT);
+		assert_eq!(VtxoState::UnregisteredBoard.as_str(), UNREGISTERED_BOARD);
+
+		// If a compiler error occurs,
+		// This is a reminder that you should update the test above
+		match VtxoState::Spent {
+			VtxoState::Ready => {},
+			VtxoState::Spent => {},
+			VtxoState::UnregisteredBoard => (),
+		}
+	}
+
+	#[test]
+	fn convert_serialize() {
+		let states = [VtxoState::Ready, VtxoState::Spent, VtxoState::UnregisteredBoard];
+
+		assert_eq!(
+			serde_json::to_string(&states).unwrap(),
+			serde_json::to_string(&[READY, SPENT, UNREGISTERED_BOARD]).unwrap(),
+		);
 
 		// If a compiler error occurs,
 		// This is a reminder that you should update the test above
