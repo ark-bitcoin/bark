d91e095f79e54f38d242612bd620eab328c19c5a
Erik De Smedt <contact@erikdesmedt.be>
2025-04-08 19:13:13 +0200

    ark-testing: Create a test to upsert a vtxo in the database
    fixup

---
+++ ark-testing/tests/postgres.rs
@@ -0,0 +1,26 @@
+use ark_testing::TestContext;
+use aspd::database::Db;
+
+use ark::test::dummy;
+
+#[tokio::test]
+async fn upsert_vtxo() {
+	let ctx = TestContext::new_minimal("postgresd/upsert_vtxo").await;
+	let (postgres_cfg, _postgresd) = ctx.new_postgres("upsert_vtxo").await;
+
+	Db::create(&postgres_cfg).await.expect("Database created");
+	let db = Db::connect(&postgres_cfg).await.expect("Connected to database");
+
+	// Create a few dummy vtxo's
+	let board_1 = dummy::DummyVtxoBuilder::new(1).build();
+	let board_2 = dummy::DummyVtxoBuilder::new(2).build();
+	let board_3 = dummy::DummyVtxoBuilder::new(3).build();
+
+	db.upsert_vtxos(&[board_1.clone(), board_2.clone()]).await.expect("Query succeeded");
+	db.get_vtxos_by_id(&[board_1.id(), board_2.id()]).await.expect("Query succeeded");
+	db.get_vtxos_by_id(&[board_3.id()]).await.expect_err("Query Failed because 3 isn't in the db yet");
+
+	// It shouldn't complain if board_2 is already present
+	db.upsert_vtxos(&[board_2, board_3]).await.expect("Query succeeded");
+}
+
