01057f0b6ddc3d2d7fe9bc0fdb9f4bdcdb6d1e07
Steven Roose <steven@stevenroose.org>
2025-04-30 17:02:13 +0100

    bitcoin-ext: Ban untrusted utxos instead of just unconfirmed

--- bitcoin-ext/src/bdk.rs
+++ bitcoin-ext/src/bdk.rs
@@ -118,11 +118,12 @@ pub trait WalletExt: BorrowMut<Wallet> {
 
 		let balance = wallet.balance().total();
 
+		let untrusted_utxos = wallet.untrusted_utxos(None);
 		let template_weight = {
 			let mut b = wallet.build_tx();
 			b.ordering(TxOrdering::Untouched);
 			b.only_witness_utxo();
-			b.only_spend_confirmed();
+			b.unspendable(untrusted_utxos.clone());
 			for anchor in &anchors {
 				b.add_fee_anchor_spend(*anchor);
 			}
@@ -158,6 +159,7 @@ pub trait WalletExt: BorrowMut<Wallet> {
 		// Then build actual tx.
 		let mut b = wallet.build_tx();
 		b.only_witness_utxo();
+		b.unspendable(untrusted_utxos);
 		b.version(3); // for 1p1c package relay
 		for anchor in &anchors {
 			b.add_fee_anchor_spend(*anchor);
