fbbdb4edf6b7e16c88ebe57745af133413136898
Erik De Smedt <contact@erikdesmedt.be>
2025-05-20 14:31:10 +0200

    ark-testing: Introduce CLEAN_SUCCESSFUL_TESTS
    The test-directory can create a good amount of data. Especially when
    esplora is used the test directory can get fairly large and take up a
    lot of disk-space.
    
    This is somewhat problematic if I am using flake_fineder.py and tests
    start failing because I run out of space.
    
    In many cases, I am only interested in the artifacts of failed tests. I
    need these artifacts for debugging
    
    I have introduced the CLEAN_SUCCESSFUL_TESTS environment variable. If
    this variable is set we will clean the test directory of every test that
    succeeded.
    
    I also adapted FLAKE_FINDER to ensure we use this setting by default.

--- ark-testing/src/context.rs
+++ ark-testing/src/context.rs
@@ -463,5 +463,17 @@ impl TestContext {
 impl Drop for TestContext {
 	fn drop(&mut self) {
 		log::info!("TestContext: Datadir is located at {:?}", self.datadir);
+
+		if std::thread::panicking() {
+			log::info!("Leave test directory intact");
+			// do nothing
+		} else {
+			if let Some(_) = std::env::var_os(crate::constants::env::CLEAN_SUCCESSFUL_TESTS) {
+				log::info!("Cleaning up {:?} because test passed and {} is set", self.datadir, crate::constants::env::CLEAN_SUCCESSFUL_TESTS);
+				std::fs::remove_dir_all(&self.datadir).unwrap();
+			} else {
+				log::info!("Leave test-directory intact because {} is not set", crate::constants::env::CLEAN_SUCCESSFUL_TESTS);
+			}
+		}
 	}
 }
