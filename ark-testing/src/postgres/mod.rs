45519f68b4ce901677e0f0779183210aef3c5ae5
Erik De Smedt <contact@erikdesmedt.be>
2025-06-07 16:09:32 +0200

    testing: Refactor postgres
    All postgres logic is currently in
    `ark-testing/src/daemon/aspd/postgres.rs`.
    
    I've decided to do a little refactor and split up the two
    execution modes in the code.
    
    In the next commits I will be moving some of the code step-by-step.

---
+++ ark-testing/src/postgres/mod.rs
@@ -0,0 +1,31 @@
+//!
+//! The testing framework will create postgres
+//! databases that are required to run aspd.
+//!
+//! It can be configured in two different methods
+//! - test-managed hosts (default)
+//! - externally hosted postgres
+//!
+//! ## Test Managed hosts
+//!
+//! The test framework will spin-up a postgres-host
+//! for each test. This requires that all postgres binaries
+//! such as `init_db` and `pg_ctl` are installed on the system.
+//!
+//! You can point the test framework to a specific installation
+//! using the `POSTGRES_BINS` environment variable. By default
+//! it will take the binaries that are available on the path.
+//!
+//! ## Externally Hosted
+//!
+//! In an externally hosted set-up the developer or (CI)
+//! runs a postgres server. The test framework will connect
+//! to this postgres host and create a separate database for
+//! every test.
+//!
+//! This method is used if the `TEST_POSTGRES_HOST` environment
+//! variable is set.
+//!
+
+pub mod external;
+pub mod test_managed;
