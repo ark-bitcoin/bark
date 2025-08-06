07d51e37e549f35173637228d5de91a95a55e5db
Steven Roose <steven@stevenroose.org>
2025-08-06 22:09:44 +0100

    Rename asp to server in README files

--- ark_demo.sh
+++ ark_demo.sh
@@ -10,7 +10,7 @@ BITCOIND_RPC_HOST=127.0.0.1
 BITCOIND_URL=http://${BITCOIND_RPC_HOST}:${BITCOIND_RPC_PORT}
 BITCOIND_COOKIE=$PWD/test/bitcoindatadir/regtest/.cookie
 
-export DB_NAME=aspdb
+export DB_NAME=bark-server-db
 export DB_USER=postgres
 export DB_PASSWORD=postgres
 
@@ -19,7 +19,7 @@ mkdir -p ${BITCOIN_DATADIR}
 
 # Define useful aliases
 alias bcli="$BITCOIN_CLI -regtest --rpcconnect=$BITCOIND_RPC_HOST --rpcport=$BITCOIND_RPC_PORT --rpccookiefile=$BITCOIND_COOKIE"
-alias aspd="cargo run --bin aspd --"
+alias captaind="cargo run --bin captaind --"
 alias bark="cargo run --bin bark --"
 alias bd="$BITCOIND -regtest -datadir=${BITCOIN_DATADIR} -server -txindex -fallbackfee=0.0002"
 
@@ -30,13 +30,13 @@ echo "-------------------------------------------"
 echo ""
 echo "This script is useful to test and demo ark "
 echo "on regtest. The script will help you to set-up"
-echo "an ASP and a couple of clients that can send"
+echo "an Ark server and a couple of clients that can send"
 echo "ark payments to each-other."
 echo ""
 echo ""
 echo "The following aliases have been defined"
 echo "- \`bd\`: To run and start \`bitcoind\`"
 echo "- \`bcli\`: Use \`bitcoin-cli\` on your \`bitcoind\`"
-echo "- \`aspd\`: Compiles and runs \`aspd\`"
+echo "- \`captaind\`: Compiles and runs \`captaind\`"
 echo "- \`bark\`: Compiles and runs \`bark\`"
 echo ""
