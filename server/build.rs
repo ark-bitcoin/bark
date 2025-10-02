201e474c56c0eb900b468b4d5eb32b5ff08f2c0d
Steven Roose <steven@roose.io>
2025-10-02 01:00:17 +0100

    Only set GIT_HASH if not already set

--- server/build.rs
+++ server/build.rs
@@ -1,12 +1,15 @@
 
+use std::env;
 use std::process::Command;
 
 
 fn main() {
-	// Get the Git commit hash
-	let output = Command::new("git").args(["rev-parse", "HEAD"]).output()
-		.expect("Failed to execute 'git rev-parse HEAD' command");
+	if env::var("GIT_HASH").is_err() {
+		// Get the Git commit hash
+		let output = Command::new("git").args(["rev-parse", "HEAD"]).output()
+			.expect("Failed to execute 'git rev-parse HEAD' command");
 
-	let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
-	println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+		let git_hash = String::from_utf8_lossy(&output.stdout).trim().to_string();
+		println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+	}
 }
