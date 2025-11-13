ed1438722e7673b40efa66313547c727eef5a70f
Duncan Dean <git@dunxen.dev>
2025-11-13 12:25:13 +0200

    lib: Add `VtxoStatusChallenge` type to authenticate vtxo status queries
    This challenge will be used by a future PR that implements the VtxoStatus
    RPC.

--- lib/src/challenges.rs
+++ lib/src/challenges.rs
@@ -13,7 +13,7 @@ use crate::lightning::PaymentHash;
 pub struct RoundAttemptChallenge([u8; 32]);
 
 impl RoundAttemptChallenge {
-	const CHALENGE_MESSAGE_PREFIX: &'static [u8; 32] = b"Ark round input ownership proof ";
+	const CHALLENGE_MESSAGE_PREFIX: &'static [u8; 32] = b"Ark round input ownership proof ";
 
 	pub fn new(value: [u8; 32]) -> Self {
 		Self(value)
@@ -37,7 +37,7 @@ impl RoundAttemptChallenge {
 		offboard_reqs: &[OffboardRequest],
 	) -> Message {
 		let mut engine = sha256::Hash::engine();
-		engine.write_all(Self::CHALENGE_MESSAGE_PREFIX).unwrap();
+		engine.write_all(Self::CHALLENGE_MESSAGE_PREFIX).unwrap();
 		engine.write_all(&self.0).unwrap();
 		engine.write_all(&vtxo_id.to_bytes()).unwrap();
 
@@ -79,6 +79,11 @@ impl RoundAttemptChallenge {
 	}
 }
 
+/// Challenge for proving ownership of a VTXO when claiming a Lightning receive.
+///
+/// This challenge combines a payment hash with the input VTXO ID to create
+/// a unique signature proving the user controls the input VTXO and is authorised
+/// as a mitigation against liquidity denial-of-service attacks.
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct LightningReceiveChallenge(PaymentHash);
 
@@ -108,7 +113,7 @@ impl LightningReceiveChallenge {
 		vtxo_keypair: Keypair,
 	) -> schnorr::Signature {
 		SECP.sign_schnorr_with_aux_rand(
-			&LightningReceiveChallenge::as_signable_message(self, vtxo_id),
+			&Self::as_signable_message(self, vtxo_id),
 			&vtxo_keypair,
 			&rand::random()
 		)
@@ -121,7 +126,64 @@ impl LightningReceiveChallenge {
 	) -> Result<(), secp256k1::Error> {
 		SECP.verify_schnorr(
 			sig,
-			&LightningReceiveChallenge::as_signable_message(self, vtxo.id()),
+			&Self::as_signable_message(self, vtxo.id()),
+			&vtxo.user_pubkey().x_only_public_key().0,
+		)
+	}
+}
+
+/// Challenge for proving ownership of a VTXO when querying its status.
+///
+/// This is the simplest challenge - it only commits to the VTXO ID itself,
+/// with no additional challenge data or context. It proves the user controls
+/// the VTXO and is authorised to query its status.
+///
+/// No additional unique or random challenge data is necessary here.
+/// We're not concerned with guarding against "replay" attacks as this challenge
+/// is for informational purposes and knowledge of this proof by a third party
+/// would indicate some kind of prior privacy leak for the user.
+///
+/// A malicious third party that can access this signed message would only be able
+/// to query the status of this specific VTXO.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
+pub struct VtxoStatusChallenge;
+
+impl VtxoStatusChallenge {
+	const CHALLENGE_MESSAGE_PREFIX: &'static [u8; 32] = b"Ark VTXO status query challenge ";
+
+	pub fn new() -> Self {
+		Self
+	}
+
+	fn as_signable_message(&self, vtxo_id: VtxoId) -> Message {
+		let mut engine = sha256::Hash::engine();
+		engine.write_all(Self::CHALLENGE_MESSAGE_PREFIX).unwrap();
+		engine.write_all(&vtxo_id.to_bytes()).unwrap();
+
+		let hash = sha256::Hash::from_engine(engine).to_byte_array();
+		Message::from_digest(hash)
+	}
+
+	pub fn sign_with(
+		&self,
+		vtxo_id: VtxoId,
+		vtxo_keypair: Keypair,
+	) -> schnorr::Signature {
+		SECP.sign_schnorr_with_aux_rand(
+			&Self::as_signable_message(self, vtxo_id),
+			&vtxo_keypair,
+			&rand::random(),
+		)
+	}
+
+	pub fn verify_input_vtxo_sig(
+		&self,
+		vtxo: &Vtxo,
+		sig: &schnorr::Signature,
+	) -> Result<(), secp256k1::Error> {
+		SECP.verify_schnorr(
+			sig,
+			&Self::as_signable_message(self, vtxo.id()),
 			&vtxo.user_pubkey().x_only_public_key().0,
 		)
 	}
