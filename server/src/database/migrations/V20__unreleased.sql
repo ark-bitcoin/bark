af32a243f213432c6eb7ee92e774705206247136
Erik De Smedt <contact@erikdesmedt.be>
2025-11-28 13:07:11 +0100

    server: Implement database utilities
    Implements some database utilities.
    
    I have created a function which
    - inserts new vtxos and can mark some (potentially other) vtxos as spend
    
    I had to update the database trigger.
    The `upsert_vtxos` and `mark_package_spent` are setting `updated_at` to
    `NOW()`. I am executing them in the same `tx` and as a consequence
    `NOW()` will be exactly the same. It is the timestamp at which the
    transaction started. I made the `updated_at` check in the database
    trigger a bit more lenient

--- server/src/database/migrations/V20__unreleased.sql
+++ server/src/database/migrations/V20__unreleased.sql
@@ -4,3 +4,26 @@
 
 ALTER TABLE vtxo_pool ADD COLUMN vtxo BYTEA NOT NULL;
 ALTER TABLE vtxo_pool DROP COLUMN depth;
+
+CREATE OR REPLACE FUNCTION vtxo_update_trigger()
+  RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO vtxo_history (
+        id, vtxo_id, vtxo, expiry, oor_spent_txid, forfeit_state, forfeit_round_id,
+        created_at, updated_at
+    ) VALUES (
+        OLD.id, OLD.vtxo_id, OLD.vtxo, OLD.expiry, OLD.oor_spent_txid, OLD.forfeit_state, OLD.forfeit_round_id,
+        OLD.created_at, OLD.updated_at
+    );
+
+    IF NEW.updated_at = OLD.updated_at AND new.updated_AT <> NOW() THEN
+        RAISE EXCEPTION 'updated_at must be updated';
+    END IF;
+
+    IF NEW.created_at <> OLD.created_at THEN
+        RAISE EXCEPTION 'created_at cannot be updated';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
