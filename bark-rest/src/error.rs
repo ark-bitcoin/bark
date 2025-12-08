085c0ed7df9566f566f53bb9e0364b8792f1131c
Steven Roose <steven@stevenroose.org>
2025-12-08 18:47:46 -0300

    bark-rest: Deduplicate error response from anyhow context
    This also fixes the error responses to contain the full error message.

--- bark-rest/src/error.rs
+++ bark-rest/src/error.rs
@@ -1,4 +1,5 @@
-use std::fmt::{self, Display};
+
+use std::fmt;
 
 use anyhow::Context;
 use axum::http::StatusCode;
@@ -7,135 +8,152 @@ use axum::Json;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct NotFoundError {
-	resource: Vec<String>,
+/// a NOT_FOUND anyhow context object
+#[derive(Debug)]
+struct NotFound {
+	resources: Vec<String>,
 	message: String,
 }
 
-impl fmt::Display for NotFoundError {
+impl fmt::Display for NotFound {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "not found: {}", self.message)
+		if self.resources.is_empty() {
+			write!(f, "not found: {}", self.message)?;
+		} else {
+			f.write_str("not found [resources=")?;
+			let mut iter = self.resources.iter().peekable();
+			while let Some(r) = iter.next() {
+				if iter.peek().is_some() {
+					write!(f, "{},", r)?;
+				} else {
+					write!(f, "{}", r)?;
+				}
+			}
+			write!(f, "]: {}", self.message)?;
+		}
+		Ok(())
 	}
 }
 
-impl NotFoundError {
-	pub fn new<V, I>(ressource: V, message: impl Display) -> Self
-	where V: IntoIterator<Item = I>, I: Display {
-		NotFoundError {
-			resource: ressource.into_iter().map(|r| r.to_string()).collect::<Vec<_>>(),
+impl NotFound {
+	fn new<I: fmt::Display>(
+		resources: impl IntoIterator<Item = I>,
+		message: impl fmt::Display,
+	) -> Self {
+		NotFound {
+			resources: resources.into_iter().map(|r| r.to_string()).collect(),
 			message: message.to_string(),
 		}
 	}
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct NotFoundError {
+	pub resources: Vec<String>,
+	pub message: String,
+}
+
 #[allow(unused)]
 macro_rules! not_found {
-	($ids:expr, $($arg:tt)*) => { return Err($crate::error::ErrorResponse::new_not_found($ids, format!($($arg)*))) };
+	($ids:expr, $($arg:tt)*) => {
+		return Err($crate::error::ErrorResponse::NotFound($crate::error::NotFoundError {
+			resources: ($ids).into_iter().map(|r| r.to_string()).collect(),
+			message: format!($($arg)*),
+		}))
+	};
 }
 pub(crate) use not_found;
 
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct BadRequestError { message: String }
 
-impl fmt::Display for BadRequestError {
+/// a BAD_REQUEST anyhow context object
+#[derive(Debug)]
+struct BadRequest {
+	message: String,
+}
+
+impl fmt::Display for BadRequest {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "bad request: {}", self.message)
 	}
 }
 
-impl BadRequestError {
-	pub fn new(message: impl Display) -> Self {
-		BadRequestError { message: message.to_string() }
+impl BadRequest {
+	pub fn new(message: impl fmt::Display) -> Self {
+		BadRequest {
+			message: message.to_string(),
+		}
 	}
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct BadRequestError {
+	pub message: String,
 }
 
 macro_rules! badarg {
-	($($arg:tt)*) => { return Err($crate::error::ErrorResponse::new_bad_request(format!($($arg)*))) };
+	($($arg:tt)*) => {
+		return Err($crate::error::ErrorResponse::BadRequest($crate::error::BadRequestError {
+			message: format!($($arg)*),
+		}))
+	};
 }
 pub(crate) use badarg;
 
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct InternalServerError { message: String }
 
-impl From<anyhow::Error> for InternalServerError {
-	fn from(err: anyhow::Error) -> Self {
-		InternalServerError { message: err.to_string() }
-	}
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct InternalServerError {
+	pub message: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+// NB since we don't do any tagging, this is not deserializable
+#[derive(Debug, Clone, Serialize, ToSchema)]
+#[serde(untagged)]
 pub enum ErrorResponse {
-	NotFound(NotFoundError),
 	BadRequest(BadRequestError),
-	InternalServerError(InternalServerError),
+	NotFound(NotFoundError),
+	Internal(InternalServerError),
 }
 
 impl ErrorResponse {
-	pub fn new_not_found<V, I>(ids: V, message: impl Display) -> Self
-	where V: IntoIterator<Item = I>, I: Display {
-		ErrorResponse::NotFound(NotFoundError::new(ids, message))
-	}
-
-	pub fn new_bad_request(message: impl Display) -> Self {
-		ErrorResponse::BadRequest(BadRequestError::new(message))
-	}
-
-	fn to_status(&self) -> StatusCode {
+	pub fn status_code(&self) -> StatusCode {
 		match self {
-			ErrorResponse::NotFound(_) => StatusCode::NOT_FOUND,
-			ErrorResponse::BadRequest(_) => StatusCode::BAD_REQUEST,
-			ErrorResponse::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::BadRequest(_) => StatusCode::BAD_REQUEST,
+			Self::NotFound(_) => StatusCode::NOT_FOUND,
+			Self::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
 		}
-	}
-
-	fn to_json(&self) -> Json<serde_json::Value> {
-		let value = match self {
-			ErrorResponse::NotFound(err) => serde_json::to_value(err).unwrap(),
-			ErrorResponse::BadRequest(err) => serde_json::to_value(err).unwrap(),
-			ErrorResponse::InternalServerError(err) => serde_json::to_value(err).unwrap(),
-		};
-		Json(value)
-	}
-}
-
-impl From<NotFoundError> for ErrorResponse {
-	fn from(err: NotFoundError) -> Self {
-		ErrorResponse::NotFound(err)
-	}
-}
-
-impl From<BadRequestError> for ErrorResponse {
-	fn from(err: BadRequestError) -> Self {
-		ErrorResponse::BadRequest(err)
-	}
-}
-
-impl From<InternalServerError> for ErrorResponse {
-	fn from(err: InternalServerError) -> Self {
-		ErrorResponse::InternalServerError(err)
 	}
 }
 
 impl From<anyhow::Error> for ErrorResponse {
-	fn from(err: anyhow::Error) -> Self {
-		if let Some(nf) = err.downcast_ref::<NotFoundError>() {
-			ErrorResponse::NotFound(nf.clone())
-		} else if let Some(ba) = err.downcast_ref::<BadRequestError>() {
-			ErrorResponse::BadRequest(ba.clone())
+	fn from(error: anyhow::Error) -> Self {
+		if let Some(c) = error.downcast_ref::<NotFound>() {
+			Self::NotFound(NotFoundError {
+				resources: c.resources.clone(),
+				message: format!("{:#}", error),
+			})
+		} else if error.is::<BadRequest>() {
+			Self::BadRequest(BadRequestError {
+				message: format!("{:#}", error),
+			})
 		} else {
-			ErrorResponse::InternalServerError(err.into())
+			Self::Internal(InternalServerError {
+				message: format!("{:#}", error),
+			})
 		}
+	}
+}
+
+impl IntoResponse for ErrorResponse {
+	fn into_response(self) -> axum::response::Response {
+		(self.status_code(), Json(self)).into_response()
 	}
 }
 
 /// Extension trait for adding bark-server-specific error info.
 pub trait ContextExt<T, E>: Context<T, E> {
-	/// Turn an anyhow error into an ErrorResponse.
 	fn badarg<C>(self, context: C) -> anyhow::Result<T>
 		where C: fmt::Display + Send + Sync + 'static;
 
-	/// Turn an anyhow error into an ErrorResponse.
 	fn not_found<I, V, C>(self, ids: V, context: C) -> anyhow::Result<T>
 	where
 		V: IntoIterator<Item = I>,
@@ -152,7 +170,7 @@ where
 	where
 		C: fmt::Display + Send + Sync + 'static,
 	{
-		self.context(BadRequestError::new(context))
+		self.context(BadRequest::new(context))
 	}
 
 	fn not_found<I, V, C>(self, ids: V, context: C) -> anyhow::Result<T>
@@ -161,23 +179,13 @@ where
 		I: fmt::Display,
 		C: fmt::Display + Send + Sync + 'static,
 	{
-		self.context(NotFoundError::new(ids, context))
-	}
-}
-
-
-impl IntoResponse for ErrorResponse {
-	fn into_response(self) -> axum::response::Response {
-		(self.to_status(), self.to_json()).into_response()
+		self.context(NotFound::new(ids, context))
 	}
 }
 
 // 404 handler for unmatched routes
-pub async fn route_not_found(path: String) -> (StatusCode, Json<serde_json::Value>) {
-	let error_response = ErrorResponse::NotFound(
-		NotFoundError::new(vec![path], "Route not found".to_string())
-	);
-	(error_response.to_status(), error_response.to_json())
+pub async fn route_not_found(path: String) -> (StatusCode, Json<String>) {
+	(StatusCode::NOT_FOUND, Json(format!("path not round: {}", path)))
 }
 
 // Convenience type alias for handlers that return anyhow::Result
