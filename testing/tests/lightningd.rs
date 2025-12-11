d8f364aaa69485ce91be218d57cea60d3e711ba1
Duncan Dean <git@dunxen.dev>
2025-12-11 08:47:23 +0200

    testing: Add test for second ln payment attempt while first in progress

--- testing/tests/lightningd.rs
+++ testing/tests/lightningd.rs
@@ -230,6 +230,41 @@ async fn another_bark_pays_invoice_after_first() {
 }
 
 #[tokio::test]
+async fn two_barks_try_to_pay_same_invoice() {
+	let ctx = TestContext::new("lightningd/two_barks_try_to_pay_same_invoice").await;
+
+	let lightning = ctx.new_lightning_setup("lightningd").await;
+
+	// Start a server and link it to our cln installation
+	let srv = ctx.new_captaind("server", Some(&lightning.sender)).await;
+
+	// Start a bark and create a VTXO
+	let bark_1 = ctx.new_bark_with_funds("bark-1", &srv, btc(4)).await;
+	let bark_2 = ctx.new_bark_with_funds("bark-2", &srv, btc(4)).await;
+	let bark_receiver = ctx.new_bark_with_funds("bark-receiver", &srv, btc(2)).await;
+
+	bark_1.board_and_confirm_and_register(&ctx, btc(3)).await;
+	bark_2.board_and_confirm_and_register(&ctx, btc(3)).await;
+	bark_receiver.board_and_confirm_and_register(&ctx, btc(1)).await;
+
+	// Create a payable invoice
+	let invoice_amount = btc(2);
+	let invoice = bark_receiver.bolt11_invoice(invoice_amount).await;
+
+	lightning.sync().await;
+
+	// Will initiate, but not wait for payment to settle.
+	bark_1.pay_lightning(invoice.invoice.clone(), None).await;
+
+	let res = bark_2.try_pay_lightning(invoice.invoice, None, true).await;
+	assert!(res.unwrap_err().to_string().contains("payment already in progress"));
+
+	assert_eq!(bark_2.offchain_balance().await.pending_lightning_send, btc(0), "pending lightning send should not have changed");
+	let vtxos = bark_2.vtxos().await;
+	assert!(!vtxos.iter().any(|v| matches!(v.state, VtxoStateInfo::Locked { .. })), "should not be any locked vtxo left");
+}
+
+#[tokio::test]
 async fn bark_pay_ln_fails_then_succeeds() {
 	let ctx = TestContext::new("lightningd/bark_pay_ln_fails_then_succeeds").await;
 
