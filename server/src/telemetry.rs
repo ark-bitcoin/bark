cac035fc886d17e1e5e53c5d1d372083d02afdb6
Steven Roose <steven@stevenroose.org>
2026-01-28 15:31:20 -0300

    server: Reduce log level of opentelemetry-otlp target
    This targets emits an empty DEBUG log every few seconds

--- server/src/telemetry.rs
+++ server/src/telemetry.rs
@@ -377,6 +377,7 @@ impl Metrics {
 			.add_directive("tokio_postgres=INFO".parse().unwrap())
 			.add_directive("tonic=INFO".parse().unwrap())
 			.add_directive("tower=INFO".parse().unwrap())
+			.add_directive("opentelemetry-otlp=INFO".parse().unwrap())
 			.add_directive("h2=INFO".parse().unwrap());
 		if let Ok(env_str) = std::env::var(S::LOG_ENV_VAR) {
 			for part in env_str.split(',') {
