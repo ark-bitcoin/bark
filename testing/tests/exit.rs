c08653e7d69aac6303a17e820cd145786d2ff53b
Steven Roose <steven@stevenroose.org>
2026-02-23 17:20:49 -0300

    testing: Change lightning exit tests to use vtxo expiry

--- testing/tests/exit.rs
+++ testing/tests/exit.rs
@@ -624,13 +624,16 @@ async fn bark_should_exit_a_failed_htlc_out_that_server_refuse_to_revoke() {
 		let htlc = bark.vtxos().await.unwrap().into_iter().find(
 			|v| v.policy_type() == VtxoPolicyKind::ServerHtlcSend
 		).unwrap();
-		htlc.policy().as_server_htlc_send().unwrap().htlc_expiry + 1
+		htlc.expiry_height() - bark.config().vtxo_refresh_expiry_threshold + 1
 	};
 	ctx.generate_blocks(desired_height - tip.height).await;
 	bark_1.sync().await;
 
 	// Should start an exit
-	assert_eq!(bark_1.list_exits().await[0].state, ExitState::Start(ExitStartState { tip_height: desired_height }));
+	assert_eq!(
+		bark_1.list_exits().await[0].state,
+		ExitState::Start(ExitStartState { tip_height: desired_height }),
+	);
 	complete_exit(&ctx, &bark_1).await;
 
 	bark_1.claim_all_exits(bark_1.get_onchain_address().await).await;
@@ -757,13 +760,16 @@ async fn bark_should_exit_a_pending_htlc_out_that_server_refuse_to_revoke() {
 		let htlc = bark.vtxos().await.unwrap().into_iter().find(
 			|v| v.policy_type() == VtxoPolicyKind::ServerHtlcSend
 		).unwrap();
-		htlc.policy().as_server_htlc_send().unwrap().htlc_expiry + 1
+		htlc.expiry_height() - bark.config().vtxo_refresh_expiry_threshold + 1
 	};
 	ctx.generate_blocks(desired_height - tip.height).await;
 	bark_1.sync().await;
 
 	// Should start an exit
-	assert_eq!(bark_1.list_exits().await[0].state, ExitState::Start(ExitStartState { tip_height: desired_height }));
+	assert_eq!(
+		bark_1.list_exits().await[0].state,
+		ExitState::Start(ExitStartState { tip_height: desired_height }),
+	);
 	complete_exit(&ctx, &bark_1).await;
 
 	bark_1.claim_all_exits(bark_1.get_onchain_address().await).await;
