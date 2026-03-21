2ac6cf3945c586ba61e2919f6268a83fa9dd5a7c
Steven Roose <steven@roose.io>
2026-03-21 18:48:31 +0000

    fuzz: Fix compilation of fuzz target for Vtxo<Full>

--- fuzz/src/bin/vtxo_decode.rs
+++ fuzz/src/bin/vtxo_decode.rs
@@ -1,8 +1,8 @@
 use honggfuzz::fuzz;
 
-use ark::encode::{ProtocolDecodingError, ProtocolEncoding};
-use ark::Vtxo;
 use ark::ServerVtxoPolicy;
+use ark::encode::ProtocolEncoding;
+use ark::vtxo::{Full, Vtxo};
 
 fn main() {
 	loop {
@@ -13,19 +13,23 @@ fn main() {
 }
 
 fn do_test(data: &[u8]) {
-	let result: Result<Vtxo<ServerVtxoPolicy>, ProtocolDecodingError> = Vtxo::deserialize(&mut data.as_ref());
+	let result = Vtxo::<Full, ServerVtxoPolicy>::deserialize(&mut data.as_ref());
 
 	if let Ok(vtxo) = result {
 		let serialized = vtxo.serialize();
 
-		let vtxo2: Vtxo<ServerVtxoPolicy> =
-			Vtxo::deserialize(&mut serialized.as_slice()).expect("re-serialization should succeed");
+		let vtxo2 = Vtxo::<Full, ServerVtxoPolicy>::deserialize(&mut serialized.as_slice())
+			.expect("re-serialization should succeed");
+		assert_eq!(vtxo, vtxo2, "vtxo roundtrip should be equal");
 
 		let serialized2 = vtxo2.serialize();
-		assert_eq!(
-			serialized, serialized2,
-			"serialization should be deterministic"
-		);
+		assert_eq!(serialized, serialized2, "serialization should be deterministic");
+
+		if let Ok(user) = vtxo.try_into_user_vtxo() {
+			let user_serialized = user.serialize();
+
+			assert_eq!(serialized, user_serialized, "user policy serialization should be equal");
+		}
 	}
 }
 
